35b39f2fa1e2ecbc78aaaa93f414e57d34608fc6
Maurice Drew <drewm1987@gmail.com>
2025-06-12 20:13:09 -0500

    got a basic login page created and also added import to app.jsx

---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+
+
+function Login () {
+  
+
+return(
+    <>
+    <h2>Login</h2>
+    <form>
+    <label>
+        Username: <input/>
+    </label>
+    <label> password: <input/>
+         </label>
+    <button> Submit </button>
+
+    </form>
+
+    </>
+
+)
+}
+
+export default Login 
