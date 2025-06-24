6b3f7d22f99bb24310fe5905a12edfc260db3544
vrouker <vrouker@gmail.com>
2025-06-24 19:41:42 -0400

    Added text to the home page about the site and updated the account products route to show all products.

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+export default function Home(){
+
+return(
+    <>
+        <h1 className="homeTitle">Wind 'n' Wing</h1>
+
+        <br/><br/>
+
+        <p>Welcome to Wind 'n' Wing! We are your one stop shop for most flying things. Please check out our products page for all of our identified flying objects (IFOs).</p>
+        <br/><br/>
+
+        <p>From top-shelf aircraft to children's toys to new pets, we have something for every interest and budget!</p>
+
+        <br/>
+
+        <p>Register for an account to place orders and leave reviews on your favorite products!</p>
+
+    </>
+)
+}
